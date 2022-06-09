44eca94daf5ba1883870a49fa72822209be0aebd
Marcello Caon <32437223+mcaon@users.noreply.github.com>
2022-06-09 19:31:07 -0300

    Update libs.sh

--- libs.sh
+++ libs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="v2.8.0"
+VERSION="v2.8.1"
 URL="https://github.com/mcaon/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
